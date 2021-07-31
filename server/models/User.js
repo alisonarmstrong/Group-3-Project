5192ad931a07e02a349880ba387eeda744ccdc72
alisonarmstrong <alarmstrong15@gmail.com>
2021-07-31 14:57:33 -0600

    typo

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
-const photoSchema = require('./photo');
+const photoSchema = require('./Photo');
 
 const userSchema = new Schema(
     {
